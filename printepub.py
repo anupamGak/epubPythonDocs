6e77a1d73805a2329d0342b523bb0d41484c69fc
anupamGak <anupamgak07@gmail.com>
2015-06-11 17:20:26 +0530

    Minor change

--- printepub.py
+++ printepub.py
@@ -6,61 +6,61 @@ reAbbr = re.compile("(?<= )[A-Z]")
 container_xml = """<?xml version="1.0"?>
 <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
   <rootfiles>
-    <rootfile full-path="OEBPS/content.opf"
-     media-type="application/oebps-package+xml" />
+	<rootfile full-path="OEBPS/content.opf"
+	 media-type="application/oebps-package+xml" />
   </rootfiles>
 </container>"""
 
 content_opf = """<?xml version='1.0' encoding='utf-8'?>
 <package xmlns="http://www.idpf.org/2007/opf" xmlns:dc="http://purl.org/dc/elements/1.1/" unique-identifier="bookid" version="2.0">
   <metadata>
-    <dc:title>%(modname)s</dc:title>
-    <dc:creator>Python  Documentation</dc:creator>
-    <dc:identifier id="bookid">urn:uuid:0cc33cbd-94e2-49c1-909a-72ae16bc2658</dc:identifier>
-    <dc:language>en-US</dc:language>
+	<dc:title>%(modname)s</dc:title>
+	<dc:creator>Python  Documentation</dc:creator>
+	<dc:identifier id="bookid">urn:uuid:0cc33cbd-94e2-49c1-909a-72ae16bc2658</dc:identifier>
+	<dc:language>en-US</dc:language>
   </metadata>
   <manifest>
-    <item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
-    <item id="cover" href="title.html" media-type="application/xhtml+xml"/>
-    <item id="content" href="%(modname)s.html" media-type="application/xhtml+xml"/>
-    <item id="css" href="styles.css" media-type="text/css"
+	<item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
+	<item id="cover" href="title.html" media-type="application/xhtml+xml"/>
+	<item id="content" href="%(modname)s.html" media-type="application/xhtml+xml"/>
+	<item id="css" href="styles.css" media-type="text/css"
   </manifest>
   <spine toc="ncx">
-    <itemref idref="cover" />
-    <itemref idref="content"/>
+	<itemref idref="cover" />
+	<itemref idref="content"/>
   </spine>
   <guide>
-    <reference href="title.html" type="title-page" title="Title"/>
+	<reference href="title.html" type="title-page" title="Title"/>
   </guide>
 </package>"""
 
 toc_ncx = """<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
-                 "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+				 "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
 <ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
   <head>
-    <meta name="dtb:uid" content="urn:uuid:0cc33cbd-94e2-49c1-909a-72ae16bc2658"/>
-    <meta name="dtb:depth" content="2"/>
-    <meta name="dtb:totalPageCount" content="0"/>
-    <meta name="dtb:maxPageNumber" content="0"/>
+	<meta name="dtb:uid" content="urn:uuid:0cc33cbd-94e2-49c1-909a-72ae16bc2658"/>
+	<meta name="dtb:depth" content="2"/>
+	<meta name="dtb:totalPageCount" content="0"/>
+	<meta name="dtb:maxPageNumber" content="0"/>
   </head>
   <docTitle>
-    <text>Python Docs</text>
+	<text>Python Docs</text>
   </docTitle>
   <navMap>
-    <navPoint id="navpoint-1" playOrder="1">
-      <navLabel>
-        <text>Book cover</text>
-      </navLabel>
-      <content src="title.html"/>
-    </navPoint>
-    <navPoint id="navpoint-2" playOrder="2">
-      <navLabel>
-        <text>%(modname)s</text>
-      </navLabel>
-      <content src="%(modname)s.html"/>
-      	%(toc)s
-    </navPoint>
+	<navPoint id="navpoint-1" playOrder="1">
+	  <navLabel>
+		<text>Book cover</text>
+	  </navLabel>
+	  <content src="title.html"/>
+	</navPoint>
+	<navPoint id="navpoint-2" playOrder="2">
+	  <navLabel>
+		<text>%(modname)s</text>
+	  </navLabel>
+	  <content src="%(modname)s.html"/>
+		%(toc)s
+	</navPoint>
   </navMap>
 </ncx>"""
 
