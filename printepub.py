e21343b2919a71171fbcc169d3b54a30c666418b
anupamGak <anupam.krishna@zippr.in>
2015-06-18 01:03:02 +0530

    Added pagebreaks in CSS

--- printepub.py
+++ printepub.py
@@ -2,6 +2,8 @@ import zipfile
 import re
 
 reAbbr = re.compile("(?<= )[A-Z]")
+reFlag = re.compile(r"<!--([a-z]+)-->")
+reCountflag = re.compie(r"<!--(\d{1,2})-->")
 
 container_xml = """<?xml version="1.0"?>
 <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
@@ -14,20 +16,21 @@ container_xml = """<?xml version="1.0"?>
 content_opf = """<?xml version='1.0' encoding='utf-8'?>
 <package xmlns="http://www.idpf.org/2007/opf" xmlns:dc="http://purl.org/dc/elements/1.1/" unique-identifier="bookid" version="2.0">
   <metadata>
-	<dc:title>%(modname)s</dc:title>
-	<dc:creator>Python  Documentation</dc:creator>
+	<dc:title>Python Reference</dc:title>
+	<dc:creator>Python Documentation</dc:creator>
 	<dc:identifier id="bookid">urn:uuid:0cc33cbd-94e2-49c1-909a-72ae16bc2658</dc:identifier>
 	<dc:language>en-US</dc:language>
   </metadata>
   <manifest>
 	<item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
 	<item id="cover" href="title.html" media-type="application/xhtml+xml"/>
-	<item id="content" href="%(modname)s.html" media-type="application/xhtml+xml"/>
-	<item id="css" href="styles.css" media-type="text/css"
+	<item id="css" href="styles.css" media-type="text/css" />
+	<!--manifest-->
   </manifest>
   <spine toc="ncx">
-	<itemref idref="cover" />
+	<itemref idref="cover" linear="no"/>
 	<itemref idref="content"/>
+	<!--spine-->
   </spine>
   <guide>
 	<reference href="title.html" type="title-page" title="Title"/>
@@ -59,8 +62,8 @@ toc_ncx = """<?xml version='1.0' encoding='utf-8'?>
 		<text>%(modname)s</text>
 	  </navLabel>
 	  <content src="%(modname)s.html"/>
-		%(toc)s
 	</navPoint>
+	<!--toc-->
   </navMap>
 </ncx>"""
 
@@ -77,19 +80,73 @@ title_html = """<!DOCTYPE html>
 
 styles_css = """
 pre {
-	background-color : #a4a4a4;
+	background-color: #a4a4a4;
+	font-family: monospace;
 	overflow: auto;
+	font-size: 0.85em;
 }
 
 .descname {
-	font-weight : bold;
-	font-size : 1.4em;
+	font-weight: bold;
+	font-size: 1.4em;
 }
+
 .section {
 	page-break-before: always;
-}"""
+}
+"""
 
-def printEpub(htmlcode="", metadata={}):
+def filler(metadata={}):
+	manifest = """<item id="module-%(count)s" href="%(modname)s.html" media-type="application/xhtml+xml"/>
+	<!--manifest-->""" % metadata
+
+	spine = """<itemref idref="module-%(count)s"/>
+	<!--spine-->""" % metadata
+
+	toc = """<navPoint id="navpoint-%(navpoint)s" playOrder="%(navpoint)s">
+	  <navLabel>
+		<text>%(modname)s</text>
+	  </navLabel>
+	  <content src="%(modname)s.html"/>
+	</navPoint>
+	<!--toc-->""" % metadata
+
+	return {
+		"manifest" : manifest,
+		"spine" : spine,
+		"toc" : toc
+	}
+
+def printEpub(htmlcode="", metadata={}, choice="y"):
+	metadata.update(filler(metadata))
+	if choice == "y":
+		epubin = zipfile.ZipFile('epubs/PyReference.epub', 'r')
+
+		content_opf = epubin.read("OEBPS/content.opf")
+		metadata['count'] = re.findall(reCountflag, content_opf)[0]
+		metadata['navpoint'] = int(metadata['count']) + 1
+		content_opf = re.sub(reCountflag, "", content_opf)
+		content_opf = re.sub(reFlag, r"%(\1)s", content_opf)
+
+		toc_ncx = epubin.read("OEBPS/toc.ncx")
+		toc_ncx = re.sub(reFlag, r"%(\1)s", toc_ncx)
+
+		content_opf = content_opf % metadata
+		toc_ncx = toc_ncx % metadata
+
+
+		epubout = zipfile.ZipFile('epubs/newfile.epub', 'w')
+
+
+
+
+
+
+
+
+
+
+
 	epub = zipfile.ZipFile('epubs/%s.epub' % metadata['modname'], 'w')
 	epub.writestr("mimetype", "application/epub+zip")
 	epub.writestr("OEBPS/%s.html" % metadata['modname'], htmlcode)
--- pyref.py
+++ pyref.py
@@ -20,26 +20,6 @@ metadata = {
 	"modname" : modname
 }
 
-sections = modpage.xpath("div/div[@class='section']")
-toc = ""
-for section in sections:
-	sectmeta = {
-		"id" : section.xpath("@id")[0],
-		"title" : section.xpath("h2/text()")[0],
-		"modname" : modname
-	}
-	sectmeta['title'] = " ".join(re.findall("[a-zA-Z]+", sectmeta['title']))
-	toc += """<navPoint>
-	  <navLabel>
-        <text>%(title)s</text>
-      </navLabel>
-      <content src="%(modname)s.html#%(id)s"/>
-    </navPoint>""" % sectmeta
-
-
-
-metadata['toc'] = toc
-
 soup = html.tostring(modpage)
 soup = re.sub(reHeadlink, "", soup)
 
